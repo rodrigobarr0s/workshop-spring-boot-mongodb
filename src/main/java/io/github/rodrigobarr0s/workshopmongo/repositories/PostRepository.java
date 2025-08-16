d5b7c3febf2d71009368e33c39b862006d0367fc
rodrigobarr0s <rodrigobarros.dev@gmail.com>
2025-08-16 10:37:47 -0300

    Consulta simples com @Query

--- src/main/java/io/github/rodrigobarr0s/workshopmongo/repositories/PostRepository.java
+++ src/main/java/io/github/rodrigobarr0s/workshopmongo/repositories/PostRepository.java
@@ -2,12 +2,21 @@ package io.github.rodrigobarr0s.workshopmongo.repositories;
 
 import io.github.rodrigobarr0s.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
-    // Query Methods
+    // Consulta personalizada usando @Query e operador $regex
+    // Busca por posts cujo título contenha o texto informado, ignorando maiúsculas/minúsculas
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
+    // Query Method automático do Spring Data
+    // Realiza a mesma busca que o metodo acima, mas usando convenção de nomes
+    // "Containing" indica que o texto pode estar em qualquer parte do título
+    // "IgnoreCase" torna a busca insensível a maiúsculas/minúsculas
     List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
